fb84700bceb91e9eddb5178c5ace6b346bda0b12
LaVieBlossom <abigail-nisbett@live.nmit.ac.nz>
2022-03-09 13:56:56 +1300

    New note

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log("Hellow World!")
+// TODO:
+
+// NOTE:
+/* When creating a app.js file ensure you have first created a src folder. Then within the src folder add untitled file. Call this file app.js
+To open the app.js file you need to go to the src folder. So type in the terminal cd src, return. Then ls, return. This will show app.js.
+Now you can go node app.js
+*/
